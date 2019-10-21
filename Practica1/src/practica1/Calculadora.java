e4a79398977114303fc4a07c07514ae793e05135
xXsamurayoneXx <55166601+xXsamurayoneXx@users.noreply.github.com>
2019-10-21 14:56:11 -0500

    Add files via upload

---
+++ Practica1/src/practica1/Calculadora.java
@@ -0,0 +1,15 @@
+package practica1;
+
+public class Calculadora {
+    public void suma (int a, int b){
+        int c;
+        c=a+b;
+        System.out.print(c);
+    }
+    public int resta (int a, int b){
+        int c;
+        c=a-b;
+        System.out.print(c);
+        return (c);
+    }
+}
---
+++ Practica1/src/practica1/practica1.java
@@ -0,0 +1,18 @@
+package practica1;
+
+
+public class practica1 {
+
+   
+    public static void main(String[] args) {
+      
+        Calculadora cal = new Calculadora ();
+        cal.suma(5, 6);
+        
+        int resta;
+        int r;
+        r= cal.resta(7, 8);
+        System.out.print(r);
+    }
+    
+}
